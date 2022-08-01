80daab2d67b75a6354a24abbff0cfa8ba96a705e
Evangel90 <oladipoevangel90@gmail.com>
2022-08-01 23:18:29 +0100

    set up redux and some components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Router, Route} from "react-router-dom"
+import Header from './containers/Header'
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello World</h1>
+     <Header />
     </div>
   );
 }
